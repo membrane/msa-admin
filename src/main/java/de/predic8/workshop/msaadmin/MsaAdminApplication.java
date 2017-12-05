7832a56764102bd5e784680870ed1f164e00f5d0
oliver <weiler@predic8.de>
2017-12-05 19:51:35 +0100

    Adjustments to deps, minor bug fixes

--- src/main/java/de/predic8/workshop/msaadmin/MsaAdminApplication.java
+++ src/main/java/de/predic8/workshop/msaadmin/MsaAdminApplication.java
@@ -1,7 +1,9 @@
 package de.predic8.workshop.msaadmin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @EnableAdminServer
